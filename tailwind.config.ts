743aa0babba02b30f762a39069ddfdeddda3b721
Mavrick91 <ray.mavrick@gmail.com>
2024-05-28 18:03:13 +0800

    Update breakpoint tailwind

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,17 +1,26 @@
 import type { Config } from "tailwindcss";
 
 const config: Config = {
-	content: ["./src/pages/**/*.{js,ts,jsx,tsx,mdx}", "./src/components/**/*.{js,ts,jsx,tsx,mdx}", "./src/app/**/*.{js,ts,jsx,tsx,mdx}"],
-	theme: {
-		extend: {
-			colors: {
-				"primary-red": "#e50914",
-				"primary-red-hover": "#c11119",
-				"primary-white": "#ffffff",
-				"primary-white-hover": "#ffffffb3",
-			},
-		},
-	},
-	plugins: [],
+  content: [
+    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+  ],
+  theme: {
+    screens: {
+      sm: "576px",
+      md: "768px",
+      lg: "992px",
+    },
+    extend: {
+      colors: {
+        "primary-red": "#e50914",
+        "primary-red-hover": "#c11119",
+        "primary-white": "#ffffff",
+        "primary-white-hover": "#ffffffb3",
+      },
+    },
+  },
+  plugins: [],
 };
 export default config;
