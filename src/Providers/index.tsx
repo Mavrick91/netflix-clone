21dc309bb1bdee24baa1f04c6015ba8d84b5de0b
Mavrick91 <ray.mavrick@gmail.com>
2024-06-08 19:26:21 +0800

    Wrap useSearchParams inside Suspense

--- src/Providers/index.tsx
+++ src/Providers/index.tsx
@@ -1,10 +1,14 @@
+import { Suspense } from "react";
+
 import { AuthProvider } from "./AuthProvider";
 import QueryClientProvider from "./QueryClientProvider";
 
 const Providers = ({ children }: { children: React.ReactNode }) => {
 	return (
 		<QueryClientProvider>
-			<AuthProvider>{children}</AuthProvider>
+			<AuthProvider>
+				<Suspense fallback={<div />}>{children}</Suspense>
+			</AuthProvider>
 		</QueryClientProvider>
 	);
 };
