e4b874719c880eb1e5e710781afd6f45d312b9eb
Mavrick91 <ray.mavrick@gmail.com>
2024-06-08 08:41:22 +0800

    Install Analytics vercel

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,6 @@
 import "./globals.css";
 
+import { Analytics } from "@vercel/analytics/react";
 import { SpeedInsights } from "@vercel/speed-insights/next";
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
@@ -23,6 +24,7 @@ export default function RootLayout({
 				<Providers>
 					{children}
 					<SpeedInsights />
+					<Analytics />
 				</Providers>
 				<div id="modal-root" />
 			</body>
